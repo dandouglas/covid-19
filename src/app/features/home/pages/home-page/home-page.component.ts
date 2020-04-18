f9f42fa6ff70e0dbe40c21c7a8ab1bccbf69b05b
Daniel Douglas <dan.douglas.01@gmail.com>
2020-04-18 15:20:36 +0100

    task(home): total cases/deaths

--- src/app/features/home/pages/home-page/home-page.component.ts
+++ src/app/features/home/pages/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeApiService } from '../../services/home-api.service';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CountryStat } from '../../../dashboard/models/country-stat';
 
 @Component({
   selector: 'cv-home-page',
@@ -9,6 +11,8 @@ import { Observable } from 'rxjs';
 })
 export class HomePageComponent implements OnInit {
   data$: Observable<any>;
+  totalDeaths$: Observable<string>;
+  totalCases$: Observable<string>;
 
   constructor(
     private homeApiService: HomeApiService,
@@ -16,6 +20,14 @@ export class HomePageComponent implements OnInit {
 
   ngOnInit() {
     this.data$ = this.homeApiService.getWorldData();
+
+    this.totalDeaths$ = this.homeApiService.getWorldData().pipe(
+      map((data: CountryStat[]) => data.reduce((total, stat) => total + stat.deaths.total, 0).toLocaleString())
+    );
+
+    this.totalCases$ = this.homeApiService.getWorldData().pipe(
+      map((data: CountryStat[]) => data.reduce((total, stat) => total + stat.cases.total, 0).toLocaleString())
+    );
   }
 
 }
