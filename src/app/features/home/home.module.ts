fcbe2b3a91571b0d55669245088fed10a17678c0
Daniel Douglas <dan.douglas.01@gmail.com>
2020-04-18 14:56:25 +0100

    task(home): add basic stat cards

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AngularMaterialModule } from '../../angular-material-module';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { DashboardModule } from '../dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { HttpClientModule } from '@angular/common/http';
     AngularMaterialModule,
     CommonModule,
     HttpClientModule,
+    NgxEchartsModule,
+    DashboardModule,
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
--- src/app/features/home/pages/home-page/home-page.component.ts
+++ src/app/features/home/pages/home-page/home-page.component.ts
@@ -3,7 +3,7 @@ import { HomeApiService } from '../../services/home-api.service';
 import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-home-page',
+  selector: 'cv-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
