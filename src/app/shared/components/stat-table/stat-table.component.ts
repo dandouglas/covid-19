31ed87c8dcabab9b96fe82843c4ef9618ae5c17f
Daniel Douglas <dan.douglas.01@gmail.com>
2020-04-22 21:09:34 +0100

    task(stat table): sort

--- src/app/shared/components/stat-table/stat-table.component.ts
+++ src/app/shared/components/stat-table/stat-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { NbTreeGridDataSource, NbTreeGridDataSourceBuilder } from '@nebular/theme';
+import { NbTreeGridDataSource, NbTreeGridDataSourceBuilder, NbSortDirection, NbSortRequest } from '@nebular/theme';
 
 @Component({
   selector: 'cv-stat-table',
@@ -16,12 +16,28 @@ export class StatTableComponent implements OnInit {
 
   dataSource: NbTreeGridDataSource<FSEntry>;
 
+  sortColumn = '';
+  sortDirection: NbSortDirection = NbSortDirection.NONE;
+
   constructor(private dataSourceBuilder: NbTreeGridDataSourceBuilder<FSEntry>) {
   }
 
 
   ngOnInit() {
     this.dataSource = this.dataSourceBuilder.create(this.data);
+  }
+
+  changeSort(sortRequest: NbSortRequest): void {
+    this.dataSource.sort(sortRequest);
+    this.sortColumn = sortRequest.column;
+    this.sortDirection = sortRequest.direction;
+  }
+
+  getDirection(column: string): NbSortDirection {
+    if (column === this.sortColumn) {
+      return this.sortDirection;
+    }
+    return NbSortDirection.NONE;
   }
 
 }
