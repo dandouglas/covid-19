fb24d306a4c330f5613ca3516dba258d589c050d
Daniel Douglas <dan.douglas.01@gmail.com>
2020-05-10 17:20:10 +0100

    task(models): uselocation type

--- src/app/features/home/store/models/home-module-state.model.ts
+++ src/app/features/home/store/models/home-module-state.model.ts
@@ -3,7 +3,7 @@ import { CountryStat, UserLocation } from '../../models/country-stat';
 export interface HomeState {
     stats: CountryStat[];
     homePageState: HomePageState;
-    userLocation: UserLocation;
+    userLocation: string;
 }
 
 export interface HomePageState {
