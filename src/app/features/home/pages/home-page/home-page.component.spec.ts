b9ab40b84128badc0468b1a3fb36600c12b84046
Daniel Douglas <dan.douglas.01@gmail.com>
2020-06-01 21:43:23 +0100

    test(home): mock services and setup testbed

--- src/app/features/home/pages/home-page/home-page.component.spec.ts
+++ src/app/features/home/pages/home-page/home-page.component.spec.ts
@@ -1,14 +1,49 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomePageComponent } from './home-page.component';
+import { of } from 'rxjs';
+import { HomeFacadeService } from '../../services/home-facade.service';
+import { HomeService } from '../../services/home.service';
+import { HomePageState } from '../../store/models/home-module-state.model';
+import { HomeDataStats } from '../../models/country-stat';
 
-describe('HomePageComponent', () => {
+fdescribe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
+  let mockHomeFacadeService;
+  let mockHomeService;
+
+  const mockHomePageState: HomePageState = {
+    initialising: false,
+    searchTerm: ''
+  };
+
+  const mockData: HomeDataStats = {
+    total: undefined,
+    tableData: [],
+    highestDeaths: undefined,
+    highestCases: undefined,
+  };
 
   beforeEach(async(() => {
+    mockHomeFacadeService = jasmine.createSpyObj('mockHomeFacadeService', {
+      dispatch: () => {},
+      getData: of(mockData),
+      getHomePageState: of(mockHomePageState),
+      getUserLocation: of('')
+    });
+
+    mockHomeService = jasmine.createSpyObj('mockHomeService', {
+      getLocation: of(undefined)
+    });
+
+
     TestBed.configureTestingModule({
-      declarations: [ HomePageComponent ]
+      declarations: [ HomePageComponent ],
+      providers: [
+        { provide: HomeFacadeService, useValue: mockHomeFacadeService },
+        { provide: HomeService, useValue: mockHomeService },
+      ]
     })
     .compileComponents();
   }));
