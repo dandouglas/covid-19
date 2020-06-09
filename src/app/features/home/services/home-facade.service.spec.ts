3a8764c6e2355a115b8475304b305c29f3c78e5c
Daniel Douglas <dan.douglas.01@gmail.com>
2020-06-09 22:35:02 +0100

    test(home): unit test facade service WIP

--- src/app/features/home/services/home-facade.service.spec.ts
+++ src/app/features/home/services/home-facade.service.spec.ts
@@ -1,11 +1,53 @@
 import { TestBed } from '@angular/core/testing';
 import { HomeFacadeService } from './home-facade.service';
+import { Store, Action } from '@ngrx/store';
+import { HomeService } from './home.service';
+import { HomeDataStats } from '../models/country-stat';
 
-describe('HomeFacade.Service.TsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('HomeFacadeService', () => {
+
+  let mockStore;
+  let mockHomeService;
+
+  beforeEach(() => {
+    mockStore = jasmine.createSpyObj('mockStore', ['dispatch']);
+    mockHomeService = jasmine.createSpyObj('mockHomeService', {
+      findAll: [],
+      nonCountryFilter: [],
+      tableDataMap: []
+    });
+  });
+
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: Store, useValue: mockStore },
+      { provide: HomeService, useValue: mockHomeService }
+    ]
+  }));
 
   it('should be created', () => {
-    const service: HomeFacadeService = TestBed.get(HomeFacadeService);
+    const service: HomeFacadeService = TestBed.inject(HomeFacadeService);
     expect(service).toBeTruthy();
   });
+
+
+  describe('dispatch', () => {
+    it('should dispatch the correct store action', () => {
+      const service: HomeFacadeService = TestBed.inject(HomeFacadeService);
+      const mockAction = { type: 'mock-action' };
+      service.dispatch(mockAction);
+      expect(mockStore.dispatch).toHaveBeenCalledWith(mockAction);
+    });
+  });
+
+  // describe('getData', () => {
+  //   it('should return an observable with the correct data', () => {
+  //     const mockData: HomeDataStats = {
+  //       total: [],
+  //       tableData: [],
+  //       highestDeaths: {},
+  //       highestCases: {}
+  //     }
+  //   });
+  // });
 });
---
+++ src/testing/country-stat-generator.ts
@@ -0,0 +1,16 @@
+// import { CountryStat } from '../app/features/home/models/country-stat';
+// import * as faker from 'faker';
+
+// export function countryStatGenerator(override?: Partial<CountryStat>): CountryStat {
+//   const countryStat: CountryStat = {
+//     country: faker.address.country()
+//     cases: CaseStats;
+//     deaths: DeathStats;
+//     day: faker.date.recent().toString(),
+//     time: faker.random.
+//   }
+// }
+
+// const mockCaseStats: CaseStats = {
+
+// }
