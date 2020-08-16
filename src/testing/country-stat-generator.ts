302569afa8d1b6149a8fee1b5a6843594d024fc3
Daniel Douglas <dan.douglas.01@gmail.com>
2020-08-16 13:35:39 +0100

    task(models): interface and generator

--- src/testing/country-stat-generator.ts
+++ src/testing/country-stat-generator.ts
@@ -1,16 +1,28 @@
-// import { CountryStat } from '../app/features/home/models/country-stat';
-// import * as faker from 'faker';
+import { CountryStat, CaseStats, DeathStats } from '../app/features/home/models/country-stat';
+import * as faker from 'faker';
 
-// export function countryStatGenerator(override?: Partial<CountryStat>): CountryStat {
-//   const countryStat: CountryStat = {
-//     country: faker.address.country()
-//     cases: CaseStats;
-//     deaths: DeathStats;
-//     day: faker.date.recent().toString(),
-//     time: faker.random.
-//   }
-// }
 
-// const mockCaseStats: CaseStats = {
+const mockCaseStats: CaseStats = {
+  new: faker.random.number().toString(),
+  active: faker.random.number(),
+  critical: faker.random.number(),
+  recovered: faker.random.number(),
+  total: faker.random.number(),
+};
 
-// }
+const mockDeathStats: DeathStats = {
+  new: faker.random.number().toString(),
+  total: faker.random.number(),
+};
+
+export function countryStatGenerator(override?: Partial<CountryStat>): CountryStat {
+  const countryStat: CountryStat = {
+    country: faker.address.country(),
+    cases: mockCaseStats,
+    deaths: mockDeathStats,
+    day: faker.date.recent().toString(),
+    time: faker.date.recent().getTime().toString()
+  };
+
+  return Object.assign(countryStat, override);
+}
