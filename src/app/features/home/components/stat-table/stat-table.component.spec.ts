12796fd6b3811fa3f159d33275a33a6091e6542f
Daniel Douglas <dan.douglas.01@gmail.com>
2020-09-12 12:58:42 +0100

    test(stat table): update tests

--- src/app/features/home/components/stat-table/stat-table.component.spec.ts
+++ src/app/features/home/components/stat-table/stat-table.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StatTableComponent } from './stat-table.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import * as faker from 'faker';
+import { NbSortDirection } from '@nebular/theme';
 
 describe('StatTableComponent', () => {
   let component: StatTableComponent;
@@ -24,6 +26,27 @@ describe('StatTableComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onChangeSort', () => {
+    it('should emit the sort request', () => {
+      const mockNbSortRequest = {
+        column: faker.random.word(),
+        direction: NbSortDirection.ASCENDING
+      };
+      component.changeSort = jasmine.createSpyObj('changeSort', ['emit']);
+      component.onChangeSort(mockNbSortRequest);
+      expect(component.changeSort.emit).toHaveBeenCalledWith(mockNbSortRequest);
+    });
+  });
+
+  describe('onGetDirection', () => {
+    it('should emit the column which has been ordered', () => {
+      const mockColumn = faker.random.word();
+      component.getDirection = jasmine.createSpyObj('getDirection', ['emit']);
+      component.onGetDirection(mockColumn);
+      expect(component.getDirection.emit).toHaveBeenCalledWith(mockColumn);
+    });
   });
 
 });
