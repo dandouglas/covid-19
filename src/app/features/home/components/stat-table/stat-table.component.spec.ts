e6cdf5a911e61b53166c58b3e92eab5d57450678
Daniel Douglas <dan.douglas.01@gmail.com>
2020-09-12 12:50:25 +0100

    test(table): fix tests

--- src/app/features/home/components/stat-table/stat-table.component.spec.ts
+++ src/app/features/home/components/stat-table/stat-table.component.spec.ts
@@ -1,96 +1,29 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { StatTableComponent } from './stat-table.component';
-// import { NbTreeGridDataSourceBuilder, NbSortDirection } from '@nebular/theme';
-// import { FSEntry, TreeNode } from '../../models/stat.models';
-// import { MockTableData } from '../../../../../testing/mock-table-data';
-// import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { StatTableComponent } from './stat-table.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-// describe('StatTableComponent', () => {
-//   let component: StatTableComponent;
-//   let fixture: ComponentFixture<StatTableComponent>;
+describe('StatTableComponent', () => {
+  let component: StatTableComponent;
+  let fixture: ComponentFixture<StatTableComponent>;
 
-//   let mockDataSourceBuilder;
 
-//   const mockTableData = MockTableData as unknown as TreeNode<FSEntry>[];
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [StatTableComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+      .compileComponents();
+  }));
 
-//   const mockColumn = 'testColumn';
-//   const mockDirection = NbSortDirection.ASCENDING;
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StatTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   const mockNbSortRequest = {
-//     column: 'testColumn',
-//     direction: mockDirection
-//   };
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   beforeEach(async(() => {
-
-//     mockDataSourceBuilder = jasmine.createSpyObj('mockDataSourceBuilder', {
-//       create: {
-//         sort: jasmine.createSpy('sort')
-//       }
-//     });
-
-//     TestBed.configureTestingModule({
-//       declarations: [StatTableComponent],
-//       providers: [
-//         { provide: NbTreeGridDataSourceBuilder, useValue: mockDataSourceBuilder }
-//       ],
-//       schemas: [CUSTOM_ELEMENTS_SCHEMA]
-//     })
-//       .compileComponents();
-//   }));
-
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(StatTableComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//     component.data = mockTableData;
-//     component.ngOnInit();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-
-//   describe('ngOnInit', () => {
-//     it('should initialise the data source', () => {
-//       expect(mockDataSourceBuilder.create).toHaveBeenCalledWith(mockTableData);
-//     });
-//   });
-
-//   describe('changeSort', () => {
-
-//     beforeEach(() => {
-//       component.changeSort(mockNbSortRequest);
-//     });
-
-//     it('should sort the data', () => {
-//       expect(component.dataSource.sort).toHaveBeenCalledWith(mockNbSortRequest);
-//     });
-
-//     it('should set the sort column correctly', () => {
-//       expect(component.sortColumn).toEqual(mockNbSortRequest.column);
-//     });
-
-//     it('should set the sortDirection property correctly', () => {
-//       expect(component.sortDirection).toBe(mockNbSortRequest.direction);
-//     });
-//   });
-
-//   describe('getDirection', () => {
-
-//     beforeEach(() => {
-//       component.changeSort(mockNbSortRequest);
-//     });
-
-//     it('should return the correct sort direction', () => {
-//       const res = component.getDirection(mockColumn);
-//       expect(res).toEqual(mockDirection);
-//     });
-
-//     it('should return NbSortDirection.NONE', () => {
-//       const res = component.getDirection('abc');
-//       expect(res).toEqual(NbSortDirection.NONE);
-//     });
-//   });
-// });
+});
--- src/app/features/home/containers/stat-table-container/stat-table-container.component.spec.ts
+++ src/app/features/home/containers/stat-table-container/stat-table-container.component.spec.ts
@@ -1,25 +1,93 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { StatTableContainerComponent } from './stat-table-container.component';
+import { StatTableContainerComponent } from './stat-table-container.component';
+import { MockTableData } from '../../../../../testing/mock-table-data';
+import { NbSortDirection, NbTreeGridDataSourceBuilder } from '@nebular/theme';
+import { FSEntry, TreeNode } from '../../models/stat.models';
 
-// describe('StatTableContainerComponent', () => {
-//   let component: StatTableContainerComponent;
-//   let fixture: ComponentFixture<StatTableContainerComponent>;
+describe('StatTableContainerComponent', () => {
+  let component: StatTableContainerComponent;
+  let fixture: ComponentFixture<StatTableContainerComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ StatTableContainerComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  let mockDataSourceBuilder;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(StatTableContainerComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  const mockTableData = MockTableData as unknown as TreeNode<FSEntry>[];
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  const mockColumn = 'testColumn';
+  const mockDirection = NbSortDirection.ASCENDING;
+
+  const mockNbSortRequest = {
+    column: 'testColumn',
+    direction: mockDirection
+  };
+
+  beforeEach(async(() => {
+    mockDataSourceBuilder = jasmine.createSpyObj('mockDataSourceBuilder', {
+      create: {
+        sort: jasmine.createSpy('sort')
+      }
+    });
+
+    TestBed.configureTestingModule({
+      declarations: [ StatTableContainerComponent ],
+      providers: [
+        { provide: NbTreeGridDataSourceBuilder, useValue: mockDataSourceBuilder }
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StatTableContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    component.data = mockTableData;
+    component.ngOnInit();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should initialise the data source', () => {
+      expect(mockDataSourceBuilder.create).toHaveBeenCalledWith(mockTableData);
+    });
+  });
+
+  describe('changeSort', () => {
+
+    beforeEach(() => {
+      component.changeSort(mockNbSortRequest);
+    });
+
+    it('should sort the data', () => {
+      expect(component.dataSource.sort).toHaveBeenCalledWith(mockNbSortRequest);
+    });
+
+    it('should set the sort column correctly', () => {
+      expect(component.sortColumn).toEqual(mockNbSortRequest.column);
+    });
+
+    it('should set the sortDirection property correctly', () => {
+      expect(component.sortDirection).toBe(mockNbSortRequest.direction);
+    });
+  });
+
+  describe('getDirection', () => {
+
+    beforeEach(() => {
+      component.changeSort(mockNbSortRequest);
+    });
+
+    it('should return the correct sort direction', () => {
+      const res = component.getDirection(mockColumn);
+      expect(res).toEqual(mockDirection);
+    });
+
+    it('should return NbSortDirection.NONE', () => {
+      const res = component.getDirection('abc');
+      expect(res).toEqual(NbSortDirection.NONE);
+    });
+  });
+});
