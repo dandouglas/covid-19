6cfe5a955a2a31cccc1db19951db6bbe61a4bc79
Daniel Douglas <dan.douglas.01@gmail.com>
2020-10-30 16:15:01 +0000

    test(home): use ng-mocks

--- src/app/features/home/pages/home-page/home-page.component.spec.ts
+++ src/app/features/home/pages/home-page/home-page.component.spec.ts
@@ -5,12 +5,15 @@ import { HomeFacadeService } from '../../services/home-facade.service';
 import { HomePageState } from '../../store/models/home-module-state.model';
 import { HomeDataStats } from '../../models/stat.models';
 import { HomePageActions } from '../../store/actions/home-page.actions';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MockComponent, MockDirective, MockService } from 'ng-mocks';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { NbCardBodyComponent, NbCardHeaderComponent, NbSpinnerDirective } from '@nebular/theme';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
-  let mockHomeFacadeService;
+
+  const mockHomeFacadeService = MockService(HomeFacadeService);
 
   const mockHomePageState: HomePageState = {
     initialising: false,
@@ -27,23 +30,23 @@ describe('HomePageComponent', () => {
   const mockLocation = 'GB';
 
   beforeEach(async(() => {
-    mockHomeFacadeService = jasmine.createSpyObj('mockHomeFacadeService', {
-      dispatch: () => {},
-      getHomePageState: of(mockHomePageState),
-      getUserLocation: of(mockLocation),
-      getStats: of(mockData)
-    });
+    mockHomeFacadeService.getHomePageState = jasmine.createSpy('getHomePageState').and.returnValue(of(mockHomePageState));
+    mockHomeFacadeService.getUserLocation = jasmine.createSpy('getUserLocation').and.returnValue(of(mockLocation));
+    mockHomeFacadeService.getStats = jasmine.createSpy('getStats').and.returnValue(of(mockData));
 
     TestBed.configureTestingModule({
-      declarations: [ HomePageComponent ],
+      declarations: [
+        HomePageComponent,
+        MockComponent(FaIconComponent),
+        MockComponent(NbCardHeaderComponent),
+        MockComponent(NbCardBodyComponent),
+        MockDirective(NbSpinnerDirective),
+      ],
       providers: [
         { provide: HomeFacadeService, useValue: mockHomeFacadeService },
       ],
-      schemas: [
-        NO_ERRORS_SCHEMA
-      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
